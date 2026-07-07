8bf98c54628bada43a233671e8ee642f0ba5414c
enzohub377 <enzoideias01@gmail.com>
2026-07-06 21:29:47 -0300

    Adiciona novas páginas de Cadastro e Home, refatora o estilo CSS e ajusta a funcionalidade de mostrar/esconder senha

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,25 @@
-let password = document.getElementById('password');
-let togglePassword = document.getElementById('toggle');
+//Cadastro e Login
+
+const Register = document.getElementById("formRegister");
+
+Register.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("Salvando Dados no Sistema...");
+  alert("Cadastro realizado com sucesso! Redirecionando...");
+  window.location.href = "http://127.0.0.1:5500/P%C3%A1gina%20Principal/index.html";
+});
+
+//Visualizar Senha
+
+let password = document.getElementById("password");
+let togglePassword = document.getElementById("toggle");
 
 function showHide() {
-    if(password.type === 'password') {
-        password.setAttribute('type', 'text');
-        togglePassword.classList.add('hide')
-    }
-    else {
-        password.setAttribute('type', 'password');
-        togglePassword.classList.remove('hide')
-    }
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    togglePassword.classList.add("hide");
+  } else {
+    password.setAttribute("type", "password");
+    togglePassword.classList.remove("hide");
+  }
 }
